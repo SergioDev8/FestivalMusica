f128beabe9249d55d0a4c294209deafd6626a356
SergioDev8 <t4u.n3utrin0@outlook.com>
2024-02-22 23:59:49 -0300

    Comenzando proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,22 @@
 const { src, dest, watch } = require("gulp");  // Extraemos la funcionalidad de gulp a nuestra variable
 //importamos sass
 const sass = require("gulp-sass")(require('sass'));
+// Importamo plumber
+const plumber = require('gulp-plumber');
 
 function css( done ) {
 
-    src('src/scss/app.scss')      // Identificar el archivo SASS
-        .pipe( sass())            //Compilarlo
-        .pipe(dest("build/css")); //Almacenarla en el disco duro
+    src('src/scss/**/*.scss')      // Identificar el archivo SASS
+        .pipe(plumber())           // Para evitar detenciones en la compilación por errores de escritura
+        .pipe( sass())            //Compilar el archivo SASS
+        .pipe(dest("build/css")); //Almacenarla en el disco duro en binario 
 
     done(); // Callback que avisa a gulp cuando llegamos al final
 
 }
 
 function dev( done ) {
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css); //** **/*.SCSS; para que observe los cambios de todos los archivos dentro de la carpeta scss */
     done();
 }
 
