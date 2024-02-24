5589490a627fabc12e233580b0ee624ab235a481
SergioDev8 <t4u.n3utrin0@outlook.com>
2024-02-24 06:19:19 -0300

    instalando y aplicando webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 /** src; para identificar un archivo
  * dest; para guardar el archivo
  */
-const { src, dest, watch } = require("gulp");  // Extraemos la funcionalidad de gulp a nuestra variable
+const { src, dest, watch, parallel } = require("gulp");  // Extraemos la funcionalidad de gulp a nuestra variable
+/** CSS */
 //importamos sass
 const sass = require("gulp-sass")(require('sass'));
-// Importamo plumber
+// Importamos plumber
 const plumber = require('gulp-plumber');
 
 function css( done ) {
@@ -18,10 +19,27 @@ function css( done ) {
 
 }
 
+async function versionWebp(done) {
+     
+    const webp = await import("gulp-webp"); // Manda a traer la dependencia instalada con "npm install --save-dev gulp-webp" desde la terminal"
+ 
+    const opciones = {
+        quality: 50 // Esto define que tanta calidad se le bajarán a las imágenes
+    }
+ 
+    src('src/img/**/*.{png,PNG,jpg,JPG}') // Busca recursivamente en todos los archivos y carpetas de la carpeta img con los formatos especificados
+        .pipe(webp.default(opciones)) // Los convierte en formato WEBP y les baja la calidad especificada
+        .pipe(dest('build/img')) // Los guarda en una nueva carpeta
+    
+    done(); // Callback que avisa a gulp cuando llegamos al final de la ejecución del script
+}
+
 function dev( done ) {
     watch('src/scss/**/*.scss', css); //** **/*.SCSS; para que observe los cambios de todos los archivos dentro de la carpeta scss */
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev =  parallel(versionWebp, dev); // nos ejecuta versionWebp y luego dev
+
